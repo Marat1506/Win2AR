61cda6341cd8c69fd72f3b26ace132e08b3742a1
Marat <maratmirzabalaev@gmail.com>
2024-12-28 01:06:09 +0300

    fds

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
 );
